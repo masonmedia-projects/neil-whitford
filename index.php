b8a3b9f35f9025bf5aa8a752583355a57af0dfc8
Andrew Mason <andrewmasonmedia@gmail.com>
2020-06-10 18:51:32 -0400

    Added discography section to home page + img

--- index.php
+++ index.php
@@ -43,7 +43,6 @@
       <div class="absolute z-0 left bg-success shape-parallelo-lg rellax" data-rellax-speed="1"></div>
       <div class="absolute z-0 left bg-dribble shape-parallelo-sm rellax" data-rellax-speed="2"></div>
       <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start">
-        <!-- <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">Guitarist.</h1> -->
         <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">
           <span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">G</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">u</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">i</span><span class="d-inline-block" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out-back">t</span><span class="d-inline-block" data-aos="zoom-in-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">a</span><span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">r</span><span class="d-inline-block" data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out-back">i</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">s</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">t</span>
         </h1>
@@ -64,7 +63,6 @@
       <div class="absolute z-0 left bg-dribble shape-square-lg rellax" data-rellax-speed="1"></div>
       <div class="absolute z-0 left bg-cream shape-square-sm rellax" data-rellax-speed="2"></div>
       <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start">
-        <!-- <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">Producer.</h1> -->
         <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">
           <span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">P</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">r</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">o</span><span class="d-inline-block" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out-back">d</span><span class="d-inline-block" data-aos="zoom-in-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">u</span><span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">c</span><span class="d-inline-block" data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out-back">e</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">r</span>
         </h1>
@@ -85,7 +83,6 @@
       <div class="absolute z-0 left bg-warning shape-triangle-lg rellax" data-rellax-speed="1"></div>
       <div class="absolute z-0 left bg-behance shape-triangle-sm rellax" data-rellax-speed="2"></div>
       <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start">
-        <!-- <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">Educator.</h1> -->
         <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">
           <span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">E</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">d</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">u</span><span class="d-inline-block" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out-back">c</span><span class="d-inline-block" data-aos="zoom-in-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">a</span><span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">t</span><span class="d-inline-block" data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out-back">o</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">r</span>
         </h1>
@@ -100,46 +97,37 @@
       </div>
     </div>
 
-    <!-- music  -->
+    <!-- disco  -->
     
-    <div class="row min-700 min--100 relative bg--img-home-hero p-5 m-0 text-light">
-      <div class="absolute z-0 left bg-warning shape-triangle-lg rellax" data-rellax-speed="1"></div>
-      <div class="absolute z-0 left bg-behance shape-triangle-sm rellax" data-rellax-speed="2"></div>
+    <div class="row min-700 min--100 relative bg--img-disco p-5 m-0 text-light">
 
-      <div class="col-lg-12 d-flex flex-column justify-content-center align-items-start">
+    <!-- shapes -->
+    <?php 
+    $shapeBig="bg-success shape-circle-lg";
+    $shapeSmall="bg-dribble shape-circle-sm";
+    include './includes/components/shape_top_component.php'
+    ?>
+      <div class="col-lg-7 d-flex flex-column justify-content-center align-items-start">
         <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">
-          <span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">M</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">u</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">s</span><span class="d-inline-block" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out-back">i</span><span class="d-inline-block" data-aos="zoom-in-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">c</span>
-
+          <span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">D</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">i</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">s</span><span class="d-inline-block" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out-back">c</span><span class="d-inline-block" data-aos="zoom-in-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">o</span><span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">g</span><span class="d-inline-block" data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out-back">r</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">a</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">p</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">h</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">y</span>    
         </h1>
-
-        <iframe class="my-3" scrolling="no" id="hearthis_at_track_4827197" width="100%" height="150" src="https://app.hearthis.at/embed/4827197/transparent_black/?hcolor=14cfa0&color=&style=2&block_size=2&block_space=0&background=1&waveform=0&cover=1&autoplay=0&css=" frameborder="0" allowtransparency allow="autoplay"><p>Listen to <a href="https://hearthis.at/neil-whitford/02-easy-refills/" target="_blank">Easy Refills - EKK</a> <span>by</span><a href="https://hearthis.at/neil-whitford/" target="_blank" >Neil Whitford</a> <span>on</span> <a href="https://hearthis.at/" target="_blank">hearthis.at</a></p></iframe>
-
-          <iframe class="mb-3" scrolling="no" id="hearthis_at_track_4827217" width="100%" height="150" src="https://app.hearthis.at/embed/4827217/transparent_black/?hcolor=14cfa0&color=&style=2&block_size=2&block_space=1&background=1&waveform=0&cover=1&autoplay=0&css=" frameborder="0" allowtransparency allow="autoplay"></iframe>
-
-            <iframe scrolling="no" id="hearthis_at_track_4827229" width="100%" height="150" src="https://app.hearthis.at/embed/4827229/transparent_black/?hcolor=14cfa0&color=&style=2&block_size=2&block_space=1&background=1&waveform=0&cover=1&autoplay=0&css=" frameborder="0" allowtransparency allow="autoplay"><p>Listen to <a href="https://hearthis.at/neil-whitford/01-georgian-bay-canyon/" target="_blank">Georgian Bay - Canyon from Soleil 2018</a> <span>by</span><a href="https://hearthis.at/neil-whitford/" target="_blank" >Neil Whitford</a> <span>on</span> <a href="https://hearthis.at/" target="_blank">hearthis.at</a></p></iframe>
       </div>
-      <!-- <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start">
-        <p class="h3 border-left pl-3" data-aos="fade-down">Neil Whitford is a prolific guitarist and multi-genre composer, songwriter and producer from Toronto, Ontario</p>
-
-        <iframe scrolling="no" id="hearthis_at_user_neil-whitford" width="100%" height="350" src="https://app.hearthis.at/neil-whitford/embed/transparent_black/?hcolor=14cfa0&color=&style=2&block_size=2&block_space=0&background=1&waveform=0&cover=1&autoplay=0&css=" frameborder="0" allowtransparency></iframe>
-
-        <iframe scrolling="no" id="hearthis_at_track_4827197" width="100%" height="150" src="https://app.hearthis.at/embed/4827197/transparent_black/?hcolor=14cfa0&color=&style=2&block_size=2&block_space=0&background=1&waveform=0&cover=1&autoplay=0&css=" frameborder="0" allowtransparency allow="autoplay"><p>Listen to <a href="https://hearthis.at/neil-whitford/02-easy-refills/" target="_blank">Easy Refills - EKK</a> <span>by</span><a href="https://hearthis.at/neil-whitford/" target="_blank" >Neil Whitford</a> <span>on</span> <a href="https://hearthis.at/" target="_blank">hearthis.at</a></p></iframe>
-
-        <a href="/" data-aos="fade-up">
-            <button class="btn btn-lg btn-outline-light rounded-0 mt-3">
-                <span class="h5">Read more</span>  
-            </button>  
+      <div class="col-lg-5 d-flex flex-column justify-content-center align-items-start">
+        <p class="h3 border-left pl-3" data-aos="fade-down">Samples from performance, production, mixing, and composition credits.</p>
+        <a href="/discography.php" data-aos="fade-up">
+          <button class="btn btn-lg btn-outline-light rounded-0 mt-3">
+              <span class="h5">Read more</span>  
+          </button>  
         </a>
-      </div> -->
+      </div>
     </div>
 
     <!-- contact -->
 
-    <div class="row min-700 min--100 relative bg--img-contact p-5 m-0 text-light">
+    <!-- <div class="row min-700 min--100 relative bg--img-contact p-5 m-0 text-light">
         <div class="absolute z-0 left bg-success shape-circle-lg rellax" data-rellax-speed="1"></div>
         <div class="absolute z-0 left bg-dribble shape-circle-sm rellax" data-rellax-speed="2"></div>
         <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start">
-        <!-- <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">Contact.</h1> -->
         <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">
           <span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">C</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">o</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">n</span><span class="d-inline-block" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out-back">t</span><span class="d-inline-block" data-aos="zoom-in-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">a</span><span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">c</span><span class="d-inline-block" data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out-back">t</span>
         </h1>
@@ -166,7 +154,7 @@
           </a>
         </span>
       </div>
-    </div>
+    </div> -->
 
   </div><!--end container-->
 
