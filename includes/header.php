1e5747665d9ea4edef369062f566f356cb1d2e2f
Andrew Mason <andrewmasonmedia@gmail.com>
2020-06-16 22:01:26 -0400

    Removed unused header links, slick etc

--- includes/header.php
+++ includes/header.php
@@ -9,9 +9,6 @@
   <link rel="manifest" href="site.webmanifest">
   <!-- <link rel="apple-touch-icon" href="icon.png"> -->
   <link rel="icon" href="/neil_favicon.png?v=1.1"> 
-  <link href="https://fonts.googleapis.com/css?family=Anton|Gayathri:400,700&display=swap" rel="stylesheet">
-  <!-- Add the slick-theme.css if you want default styling -->
-  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link rel="stylesheet" href="/assets/css/aos.css">
   <link rel="stylesheet" href="/assets/css/normalize.css">
