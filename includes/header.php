ea5831dcf85591250e9290dfcdacf8e976d21146
Andrew Mason <andrewmasonmedia@gmail.com>
2020-06-17 10:42:38 -0400

    Created header_social file for social meta tags, added a screenshot of homepage for social tags

--- includes/header.php
+++ includes/header.php
@@ -4,11 +4,18 @@
 <head>
   <meta charset="utf-8">
   <title><?php $title; ?></title>
-  <meta name="description" content="">
+  <meta name="description" content="Neil Whitford is a prolific guitarist, producer and dedicated music professional from Toronto, Ontario.">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="manifest" href="site.webmanifest">
   <!-- <link rel="apple-touch-icon" href="icon.png"> -->
   <link rel="icon" href="/neil_favicon.png?v=1.1"> 
+  <meta itemprop="name" content="Neil Whitford">
+  <meta itemprop="description" content="Neil Whitford is a prolific guitarist, producer and dedicated music professional from Toronto, Ontario.">
+  <meta itemprop="image" content="http://neilwhitford.com/assets/img/neil_whitford_home_ss.png">
+  <!-- fb and tw og tags -->
+  <?php include 'header_social.php'; ?>
+  <meta name="robots" content="index,follow"><!-- All Search Engines -->
+  <meta name="googlebot" content="index,follow"><!-- Google Specific -->
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link rel="stylesheet" href="/assets/css/aos.css">
   <link rel="stylesheet" href="/assets/css/normalize.css">
---
+++ includes/header_social.php
@@ -0,0 +1,22 @@
+<!-- facebook -->
+<meta property="fb:app_id" content="">
+<meta property="og:url" content="http://neilwhitford.com">
+<meta property="og:type" content="neilwhitford.com">
+<meta property="og:title" content="Neil Whitford: Musician, Producer, Educator.">
+<meta property="og:image" content="http://neilwhitford.com/assets/img/neil_whitford_home_ss.png">
+<meta property="og:image:alt" content="A screenshot of the homepage of neilhitford.com showing the website title (Neil Whitford: Guitarist, Producer, Educator), and a background image of Neil Whitford performing on guitar.">
+<meta property="og:description" content="Neil Whitford is a prolific guitarist, producer and dedicated music professional from Toronto, Ontario.">
+<meta property="og:site_name" content="Neil Whitford">
+<meta property="og:locale" content="en_US">
+<meta property="article:author" content="">
+
+<!-- twitter -->
+<meta name="twitter:card" content="summary">
+<!-- <meta name="twitter:site" content="@site_account"> -->
+<!-- <meta name="twitter:creator" content="@individual_account"> -->
+<meta name="twitter:url" content="http://neilwhitford.com">
+<meta name="twitter:title" content="Neil Whitford">
+<meta name="twitter:description" content="Neil Whitford is a prolific guitarist, producer and dedicated music professional from Toronto, Ontario.">
+<meta name="twitter:image" content="http://neilwhitford.com/assets/img/neil_whitford_home_ss.png">
+<meta name="twitter:image:alt" content="A screenshot of the homepage of neilhitford.com showing the website title (Neil Whitford: Guitarist, Producer, Educator), and an image of Neil Whitford performing on guitar.">
+<meta name="twitter:dnt" content="on">
