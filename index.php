0240439fa6f8f80ebc9a7ea0e17f16b883aeb7d3
Andrew Mason <andrewmasonmedia@gmail.com>
2020-06-16 22:37:51 -0400

    Edit to home disco section copy

--- index.php
+++ index.php
@@ -85,44 +85,10 @@ include 'includes/header.php'; ?>
     $shapeSmall="bg-dribble shape-circle-sm";
     $sectionTitle="Credits.";
     $accentColor="text-dribble";
-    $sectionText="A discography of my work with samples from performance, production, mixing, and composition credits.";
+    $sectionText="A discography of Neil's work with samples from performance, production, mixing, and composition credits.";
     $sectionLink="/discography";
     include 'includes/components/home_banner_component.php';
     ?>
-
-    <!-- contact -->
-
-    <!-- <div class="row min-700 min--100 relative bg--img-contact p-5 m-0 text-light">
-        <div class="absolute z-0 left bg-success shape-circle-lg rellax" data-rellax-speed="1"></div>
-        <div class="absolute z-0 left bg-dribble shape-circle-sm rellax" data-rellax-speed="2"></div>
-        <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start">
-        <h1 class="display-1 font-weight-bold m-0" data-aos="fade-right">
-          <span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">C</span><span class="d-inline-block" data-aos="fade-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">o</span><span class="d-inline-block" data-aos="fade" data-aos-duration="800" data-aos-easing="ease-in-out-back">n</span><span class="d-inline-block" data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out-back">t</span><span class="d-inline-block" data-aos="zoom-in-right" data-aos-duration="800" data-aos-easing="ease-in-out-back">a</span><span class="d-inline-block" data-aos="fade-down" data-aos-duration="800" data-aos-easing="ease-in-out-back">c</span><span class="d-inline-block" data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-out-back">t</span>
-        </h1>
-      </div>
-      <div class="col-lg-6 d-flex flex-row justify-content-start align-items-center">
-        <span class="display-1 d-inline-block stroke-white pr-5" data-aos="fade-down">
-          <a class="a-stroke" href="https://www.facebook.com/neil.whitford.58" target="_blank">
-            <i class="fa fa-facebook"></i>
-        </a>
-        </span>
-        <span class="display-1 d-inline-block stroke-white pr-5" data-aos="fade-up">
-          <a class="a-stroke" href="https://www.instagram.com/whitfordguitar/" target="_blank">
-            <i class="fa fa-instagram"></i>
-          </a>
-        </span>
-        <span class="display-1 d-inline-block stroke-white pr-5" data-aos="zoom-in">
-          <a class="a-stroke" href="https://www.linkedin.com/in/neil-whitford-627ab9a9/?originalSubdomain=ca" target="_blank">
-            <i class="fa fa-linkedin"></i>
-          </a>
-        </span>
-        <span class="display-1 d-inline-block stroke-white pr-5" data-aos="fade-right">
-          <a class="a-stroke" href="mailto:whitfordguitar@gmail.com" target="_blank">
-            <i class="fa fa-envelope"></i>
-          </a>
-        </span>
-      </div>
-    </div> -->
 
   </div><!--end container-->
 
